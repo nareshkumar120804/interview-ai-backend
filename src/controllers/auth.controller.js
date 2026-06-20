dcdad39386f3aeaa336b63559fa434475711562d
nareshkumar120804 <nareshkumar120804@gmail.com>
2026-06-21 05:25:30 +0530

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -44,8 +44,8 @@ async function registerUserController(req, res) {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax",
-      secure: true // true in production,
+      sameSite: "none",
+      secure: true ,// true in production
       maxAge: 24 * 60 * 60 * 1000
     });
 
@@ -106,8 +106,8 @@ async function loginUserController(req, res) {
 
     res.cookie("token", token, {
         httpOnly: true,
-        sameSite: "lax",
-        secure: true // true in production,
+        sameSite: "none",
+        secure: true ,// true in production
       maxAge: 24 * 60 * 60 * 1000
     });
 
@@ -136,7 +136,7 @@ async function logoutUserController(req, res) {
 
     res.clearCookie("token", {
         httpOnly: true,
-        sameSite: "lax",
+        sameSite: "none",
         secure: true // true in production
     });
 
